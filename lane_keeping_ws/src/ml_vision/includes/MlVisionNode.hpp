ea87d93dc5c814ce52357db29b777e9e0bbcc7d5
Axel-ex <axel.chabrerie@gmail.com>
2025-04-09 15:52:05 +0100

    Compile with Nvidia Infer header

--- lane_keeping_ws/src/ml_vision/includes/MlVisionNode.hpp
+++ lane_keeping_ws/src/ml_vision/includes/MlVisionNode.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <NvInfer.h>
 #include <lane_msgs/msg/lane_positions.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
@@ -16,4 +17,8 @@ class MlVisionNode : public rclcpp::Node
             lane_pos_pub_;
 
         void rawImageCallback(sensor_msgs::msg::Image::SharedPtr img);
+        nvinfer1::IExecutionContext* context_;
+        cudaStream_t stream_;
+        void* d_input_;
+        void* d_output;
 };
