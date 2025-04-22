0a138e87258072e75949aa274335e9baae6df824
Axel-ex <axel.chabrerie@gmail.com>
2025-04-22 10:36:10 +0100

    increase camera fps

--- lane_keeping_ws/src/camera/src/CameraNode.cpp
+++ lane_keeping_ws/src/camera/src/CameraNode.cpp
@@ -11,7 +11,7 @@ CameraNode::CameraNode() : Node("camera_node"), running_(true)
 {
     std::string pipeline =
         "nvarguscamerasrc ! video/x-raw(memory:NVMM), format=NV12, width=640, "
-        "height=480, framerate=15/1 ! nvvidconv ! video/x-raw, format=BGRx ! "
+        "height=480, framerate=30/1 ! nvvidconv ! video/x-raw, format=BGRx ! "
         "videoconvert ! video/x-raw, format=BGR ! appsink";
     cap_ = VideoCapture(pipeline, cv::CAP_GSTREAMER);
     if (!cap_.isOpened())
@@ -44,10 +44,9 @@ void CameraNode::initPublisherAndCapture()
 void CameraNode::captureFrame()
 {
     Mat frame(Size(1280, 720), CV_8UC3);
-
     while (rclcpp::ok() && running_)
     {
-        if (cap_.read(frame)) // Non-blocking read
+        if (cap_.read(frame))
         {
             std_msgs::msg::Header hdr;
             sensor_msgs::msg::Image::SharedPtr msg;
